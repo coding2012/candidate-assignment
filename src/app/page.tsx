02de9414d85e606ed4d5f51abd54c40386eb6ffe
Ravi Luthra <coding@gmail.com>
2024-09-27 17:04:54 -0700

    Link from root

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Home() {
+  return (
+    <main style={{ margin: "24px" }}>
+      <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <a href="/search">Find Solace Advocate!</a>
+      </div>
+    </main>
+  );
+}
